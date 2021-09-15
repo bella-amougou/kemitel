0c7b891e835d8c8897142daef50899dd2646e0cb
bella-amougou <84756219+bella-amougou@users.noreply.github.com>
2021-09-15 10:07:56 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 setTimeout(function () {
   $(".loader").fadeToggle();
 }, 500);
+$(document).ready(function() {
+  /* ... */
+  var windowHeight = $(window).innerHeight();
+  $('body').css({'height':windowHeight});
+  /* ... */
+});
